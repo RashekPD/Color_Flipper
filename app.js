7609d5f38c5c904e302e1dd1b4f7de8b38812ee5
RashekPD <pdsguillermojavier@gmail.com>
2023-11-22 02:08:48 +0000

    implementada funcionalidad de boton

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 let BTN = document.getElementById('btn-color-changer');
 let COLOR = document.querySelector('.color');
 
-console.log(getRandomColor())
+BTN.addEventListener("click",function(){
+    let newBackgroundColor = getRandomColor();
+    changeBackgroundColor(newBackgroundColor);
+    COLOR.textContent = newBackgroundColor;
+    COLOR.style.color = newBackgroundColor;
+})
+
+function changeBackgroundColor(color){
+    document.body.style.backgroundColor=color;
+}
+
 
 function getRandomColor(){
     return "rgb("+get255()+","+get255()+","+get255()+")";
