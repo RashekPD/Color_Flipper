a9d940414e5e1dbc11db0efe9c6ce2d855dddf81
RashekPD <pdsguillermojavier@gmail.com>
2023-11-22 02:53:08 +0000

    implementado boton de random mode

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 let BTN = document.getElementById('btn-color-changer');
 let COLOR = document.querySelector('.color');
+let random_btn = document.getElementById('random-btn')
+let RANDOMMODE = false;
 
-BTN.addEventListener("click",function(){
-    let newBackgroundColor = getRandomColor();
-    changeBackgroundColor(newBackgroundColor);
-    COLOR.textContent = newBackgroundColor;
-    COLOR.style.color = newBackgroundColor;
+BTN.addEventListener("click",changeBackgroundColor)
+random_btn.addEventListener('click',function(){
+    RANDOMMODE=!RANDOMMODE;
+    randomMode();
 })
 
-function changeBackgroundColor(color){
-    document.body.style.backgroundColor=color;
+function changeBackgroundColor(){
+    let newBackgroundColor = getRandomColor();
+    document.body.style.backgroundColor=newBackgroundColor;
+    COLOR.textContent = newBackgroundColor;
+    COLOR.style.color = newBackgroundColor;
 }
 
 
@@ -19,4 +23,11 @@ function getRandomColor(){
 
 function get255(){ // get random number from 0-255
     return Math.floor(Math.random()*256);
+}
+
+function randomMode(){
+    if(RANDOMMODE){
+        changeBackgroundColor();
+        setTimeout(randomMode,3000);
+    }
 }
